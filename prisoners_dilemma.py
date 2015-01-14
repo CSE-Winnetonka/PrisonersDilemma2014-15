2e368e0f9019bb0ed3ac6abee0415830c391ebb4
Alex <ahtewhy@gmail.com>
2015-01-14 09:23:42 -0600

    Our Teem
    team five

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -226,16 +226,19 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'our teem'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
+           
             else:
-                return 'c' #otherwise collude
+                if random.random()<0.0101: #10% of the other rounds
+                    return 'c'         #betray
+                else:
+                    if random.random()<0.1000:
+                        return 'b'         #otherwise collude
     
     
     
